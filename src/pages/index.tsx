996385d4c75a320238c633b18da7d1228662e7a9
LucasFritschy <lucasfritschy@gmail.com>
2021-05-13 10:26:01 -0300

    config do prismic e estilizacao da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -21,16 +26,72 @@ interface PostPagination {
 }
 
 interface HomeProps {
-  postsPagination: PostPagination;
+  postsPagination?: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <img src="/Logo.svg" alt="logo" />
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+        {postsPagination.results.map(result => (
+          <div className={styles.postPreview}>
+            <h2>{result.data.title}</h2>
+            <p>{result.data.subtitle}</p>
+            <div>
+              <time>
+                <FiCalendar /> {result.first_publication_date}
+              </time>
+              <span>
+                <FiUser />
+                {result.data.author}
+              </span>
+            </div>
+          </div>
+        ))}
+        {postsPagination.next_page ? (
+          <button className={styles.loadButton} type="button">
+            Carregar mais posts
+          </button>
+        ) : null}
+      </main>
+    </>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author', 'posts.slug'],
+      pageSize: 10,
+    }
+  );
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results.map(result => {
+      return {
+        uid: result.uid,
+        first_publication_date: format(
+          new Date(result.first_publication_date),
+          'dd MMM yyyy'
+        ),
+        data: {
+          title: result.data.title,
+          subtitle: result.data.subtitle,
+          author: result.data.author,
+        },
+      };
+    }),
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
